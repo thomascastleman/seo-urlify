2058465cb5414e114d75ebbc9a7273b58b96849f
Thomas Castleman <thomas7rex@gmail.com>
2020-01-20 17:37:03 -0500

    Fixed bug with not providing the option delimiter

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 	Generates a hyphenated, lower case, URL-safe
 	version of the given string, using '-' as a 
 	delimiter if none is given. */
-const format = function(s, options) {
+const format = (s, options) => {
 	// remove all non-word/non-space characters
 	s = s.replace(/[^\s\w]/g, '');
 
@@ -18,8 +18,10 @@ const format = function(s, options) {
 		s = s.toLowerCase();
 	}
 
+	const delim = options && options.delim ? options.delim : '-';
+
 	// split into word tokens and join with delimiter character (either passed as arg or default)
-	return s.split(' ').join(options.delim || '-');
+	return s.split(' ').join(delim);
 }
 
 module.exports = format;
